104f967aebaed65a5b09295f13950eca0d66f287
DILEEP V <dileepvait@gmail.com>
2026-05-17 01:45:56 +0530

    Updated premium portfolio website UI

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Main wedding platform loaded");
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+
+  if(window.scrollY > 50){
+    navbar.style.background = "rgba(255,255,255,0.92)";
+    navbar.style.boxShadow = "0 10px 30px rgba(0,0,0,0.08)";
+  } else {
+    navbar.style.background = "rgba(255,255,255,0.75)";
+    navbar.style.boxShadow = "none";
+  }
+});
